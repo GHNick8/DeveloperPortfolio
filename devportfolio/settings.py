7f425a2a4d6064b8d9f9d6baea1260db50b179ac
gitcommander78 <rawappeal@hotmail.com>
2025-02-18 11:20:49 +0100

    Updating project

--- devportfolio/settings.py
+++ devportfolio/settings.py
@@ -67,7 +67,7 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 
 if DATABASE_URL:
     DATABASES = {
-        'default': dj_database_url.config(default=DATABASE_URL)
+        'default': dj_database_url.config(default=os.getenv("DATABASE_URL"))
     }
 else:
     DATABASES = {
